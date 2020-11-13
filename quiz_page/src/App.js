926243eace3ec754ad4eac37664845d061d19d87
Samantha Jimenez <samanthajimenez@pursuit.org>
2020-11-13 08:50:26 -0500

    create routes and import components

--- quiz_page/src/App.js
+++ quiz_page/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Switch } from 'react-router-dom';
+import Err from './components/Error';
+import Footer from './components/Footer';
+import Landing from './components/Landing';
+import NavBar from './components/NavBar';
+import Quizzes from './components/Quizzes';
+import Music from './components/Music';
+import Movies from './components/Movies';
+import Love from './components/Love';
+import Food from './components/Food';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+      <Switch>
+        <Route exact path={"/"}>
+          <Landing/>
+        </Route>
+        <Route exact path={"/quizzes"}>
+          <Quizzes/>
+        </Route>
+        <Route exact path={"/quizzes/food"}>
+          <Food/>
+        </Route>
+        <Route exact path={"/quizzes/love"}>
+          <Love/>
+        </Route>
+        <Route exact path={"/quizzes/movies"}>
+          <Movies/>
+        </Route>
+        <Route exact path={"/quizzes/music"}>
+          <Music/>
+        </Route>
+        <Route path={"*"}>
+          <Err/>
+        </Route>
+      </Switch>
+      <Footer/>
     </div>
   );
 }
