db72b80b4e6209735ef7a4b2a523950195e2015e
Samantha Jimenez <samanthajimenez@pursuit.org>
2020-11-13 08:47:21 -0500

    create error page

---
+++ quiz_page/src/components/Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Err = () => {
+    return (
+        <div>
+            <p>Oops! Seems like you're lost. Head back over to the home page.</p>
+        </div>
+    )
+};
+
+export default Err;
